58b551eddf1a55272dc9942e3e134a96ce78de34
robertboksa <robert.boksa@gmail.com>
2021-10-04 17:35:43 +0200

    Entity Category, Donation, Institution done.

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,20 @@
+package pl.coderslab.charity.model;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Donation> donations = new ArrayList<>();
+}
